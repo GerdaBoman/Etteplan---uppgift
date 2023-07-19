361f3445e4b63c3ba9694e7b5c1b0a8bbd03b22d
Gerda Boman <bomangerda@gmail.com>
2023-07-19 14:35:15 +0200

    Add project files.

---
+++ Etteplan/Program.cs
@@ -0,0 +1,24 @@
+﻿using Etteplan;
+using System.Xml;
+
+int attrId = 42007;
+
+string filePath = @"C:\Users\gerda\source\repos\Etteplan\Etteplan\Data\sma_gentext.xml";
+
+
+XmlDocument xmlDoc = new XmlDocument();
+xmlDoc.Load(filePath);
+
+var node = xmlDoc.SelectSingleNode($"//*[@id='{attrId}']");
+
+
+
+UnitEntity unit = new UnitEntity
+{
+    Id = attrId,
+    Source = node.FirstChild.InnerText,
+    Target = node.LastChild.InnerText,
+};
+
+Console.WriteLine(unit.Target);
+
---
+++ Etteplan/UnitEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Etteplan
+{
+    public class UnitEntity
+    {
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public string Target { get; set; }
+    }
+}
