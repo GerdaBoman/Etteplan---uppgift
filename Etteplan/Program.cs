bb706abd43554e761e2c6e3746715f0276693c0d
Gerda Boman <bomangerda@gmail.com>
2023-07-19 15:52:37 +0200

    Refractoring

--- Etteplan/Program.cs
+++ Etteplan/Program.cs
@@ -10,21 +10,28 @@ internal class Program
         string filePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
             @"..\..\..\Data\sma_gentext.xml"));
 
+        var targetUnit = GetTargetById(filePath, attrId);
+
+        WriteToFile(targetUnit);
+    }
+
+    static UnitEntity GetTargetById(string filePath, int id)
+    {
 
         XmlDocument xmlDoc = new XmlDocument();
         xmlDoc.Load(filePath);
 
-        var node = xmlDoc.SelectSingleNode($"//*[@id='{attrId}']");
+        var node = xmlDoc.SelectSingleNode($"//*[@id='{id}']");
 
 
         UnitEntity unit = new UnitEntity
         {
-            Id = attrId,
+            Id = id,
             Source = node.FirstChild.InnerText,
             Target = node.LastChild.InnerText,
         };
 
-        WriteToFile(unit);
+        return unit;
     }
 
     static void WriteToFile(UnitEntity unit)
